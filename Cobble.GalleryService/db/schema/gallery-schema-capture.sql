98826759bf3822908f5052213b7b3ac11b734006
Angus Clark <angusclark@AR-234.local>
2026-05-04 12:34:37 +1000

    Add gallery schema capture

---
+++ Cobble.GalleryService/db/schema/gallery-schema-capture.sql
@@ -0,0 +1,3 @@
+Sqlcmd: Error: Microsoft ODBC Driver 13 for SQL Server : Login timeout expired.
+Sqlcmd: Error: Microsoft ODBC Driver 13 for SQL Server : TCP Provider: Error code 0x2749.
+Sqlcmd: Error: Microsoft ODBC Driver 13 for SQL Server : A network-related or instance-specific error has occurred while establishing a connection to SQL Server. Server is not found or not accessible. Check if instance name is correct and if SQL Server is configured to allow remote connections. For more information see SQL Server Books Online..
